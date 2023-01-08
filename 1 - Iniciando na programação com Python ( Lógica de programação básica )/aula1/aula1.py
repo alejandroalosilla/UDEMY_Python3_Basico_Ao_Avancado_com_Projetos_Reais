4b2590fe239d1eb8be50a5c12a16139e53a8d246
Alejandro Alosilla <alejandroalosilla@gmail.com>
2023-01-08 01:01:16 -0300

    Create aula1.py

---
+++ 1 - Iniciando na programação com Python ( Lógica de programação básica )/aula1/aula1.py
@@ -0,0 +1,17 @@
+"""
+Isso se chama DocString
+E escrever o que eu 
+quiser
+
+ISSO O INTERPRETADOR LÊ MAS NÃO FAZ NADA!
+"""
+'''
+Aspas simples também funcionam
+'''
+
+# Isso se chama Comentário
+# ISSO O INTERPRETADOR NÃO LÊ!
+# Permite escrever um comentário
+print(123) # Na frente
+# Abaixo
+print(456)

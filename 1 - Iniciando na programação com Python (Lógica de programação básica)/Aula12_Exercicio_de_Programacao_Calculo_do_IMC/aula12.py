8c6559b9c046f0c59616cdedfb192d026c524e48
Alejandro Alosilla <alejandroalosilla@gmail.com>
2023-01-11 19:57:14 -0300

    up 5

---
+++ 1 - Iniciando na programação com Python (Lógica de programação básica)/Aula12_Exercicio_de_Programacao_Calculo_do_IMC/aula12.py
@@ -0,0 +1,11 @@
+nome = 'Luiz Otávio'
+idade = ...  # placeholder
+altura = 1.80
+peso = 95
+imc = peso / altura ** 2
+
+
+print(f'{nome} tem {altura} de altura, '
+f'pesa {95} quilos e seu IMC é '
+f'{imc}')
+print(...)  # placeholder

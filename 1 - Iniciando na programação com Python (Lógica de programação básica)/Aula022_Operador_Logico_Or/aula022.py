31c1316493258905c4c16b8245e26967d029c3db
Alejandro Alosilla <alejandroalosilla@gmail.com>
2023-01-16 19:45:36 -0300

    up 10

--- 1 - Iniciando na programação com Python (Lógica de programação básica)/Aula022_Operador_Logico_Or/aula022.py
+++ 1 - Iniciando na programação com Python (Lógica de programação básica)/Aula022_Operador_Logico_Or/aula022.py
@@ -13,10 +13,13 @@ entrada = input('[E]ntrar [S]air: ')
 senha_digitada = input('Senha: ')
 
 senha_permitida = '123456'
-if entrada == 'E' and senha_digitada == senha_permitida:
+if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
     print('Entrar')
 else:
     print('Sair')
 
 # Avaliação de curto circuito
 print(True and False and True)
+print(False or True)
+senha = input('Senha: ') or 'Sem senha'
+print(senha)

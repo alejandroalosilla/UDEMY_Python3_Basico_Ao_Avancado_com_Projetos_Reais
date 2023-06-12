7243b0ab3b43914eeff9865e1100d19e7e30c160
Alejandro Alosilla <alejandroalosilla@gmail.com>
2023-06-12 16:39:15 -0300

    Create aula036.py

---
+++ 1 - Iniciando na programação com Python (Lógica de programação básica)/Aula036_Operadores_de_Atribuicao_com_Operadores_Aritmeticos/aula036.py
@@ -0,0 +1,16 @@
+"""
++= -= *= /= //= **= %= (shorthand)
+"""
+
+contador = 1
+
+contador += 1
+print(contador)
+contador -= 1
+print(contador)
+contador /= 1
+print(contador)
+contador //= 1
+print(contador)
+contador %= 1
+print(contador)
